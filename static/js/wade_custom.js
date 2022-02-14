fa944676cebadb81da104e5349b54f0f6256a2c4
wade <wade0702@gmail.com>
2022-02-14 16:08:08 +0800

    fix medium-zoom background code in wade_custom.js

--- static/js/wade_custom.js
+++ static/js/wade_custom.js
@@ -1,6 +1,6 @@
 mediumZoom(document.querySelectorAll('img'), {
 	margin: 24,
 	//background: '#292d3d',
-    background: 'rgba(41,45,61,0.7)'
+    background: 'rgba(41, 45, 61, 0.7)',
 	scrollOffset: 0,
 });
